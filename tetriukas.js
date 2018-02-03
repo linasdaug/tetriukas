0da83f27f11834b241464224ad9ee89c05df4776
linasdaug <34643935+linasdaug@users.noreply.github.com>
2018-02-03 20:37:12 +0200

    Add files via upload

--- tetriukas.js
+++ tetriukas.js
@@ -41,6 +41,7 @@ for (let i = 0; i < eil; i++) {
     }
 }
 
+
 function lentele() {
     let lentele = $("<table>")
     let eilute, langelis
@@ -76,7 +77,6 @@ function gautiKalade() {
 }
 
 function padetiKalade(kalade, spalva) {
-    console.log("dedam kalade");
     for (let i = 0; i < kalade.length; i++) {
         let id = kalade[i];
         $("#"+id).addClass("juda");
@@ -112,7 +112,8 @@ function zaidimas() {
     document.onkeydown = rodykles.event;
     rodykles.add(rodykles.desinen, desinen);
     rodykles.add(rodykles.kairen, kairen);
-    rodykles.add(rodykles.aukstyn, versti)
+    rodykles.add(rodykles.aukstyn, versti);
+    rodykles.add(rodykles.zemyn, leistis);
 
 }
 
@@ -126,7 +127,6 @@ function desinen () {
         let j = $(".juda").attr("id");
         $("#"+j).removeClass("juda");
         let s = $("#"+j).attr("class");
-        $("#"+j).removeClass("juda");
         $("#"+j).removeClass(s);
         j = parseInt(j);
         judaX.push(j);
@@ -151,7 +151,6 @@ function desinen () {
     padetiKalade(judaY, sp);
 }
 
-
 function kairen () {
     let judaX = [];
     let judaY = [];
@@ -162,7 +161,6 @@ function kairen () {
         let j = $(".juda").attr("id");
         $("#"+j).removeClass("juda");
         let s = $("#"+j).attr("class");
-        $("#"+j).removeClass("juda");
         $("#"+j).removeClass(s);
         j = parseInt(j);
         judaX.push(j);
@@ -188,64 +186,132 @@ function kairen () {
 }
 
 function versti () {
-
-    console.log("verciam");
     let judaX = [];
     let judaY = [];
     let sp;
     let kaladesNr;
     let pos = 179;   /* paskutinis įmanomas */
     let prieKrasto = false;
-
     for (i = 0; i < 4; i++) {
         let j = $(".juda").attr("id");
         $("#"+j).removeClass("juda");
         let s = $("#"+j).attr("class");
-        $("#"+j).removeClass("juda");
         $("#"+j).removeClass(s);
         j = parseInt(j);
         judaX.push(j);
         if (i == 0) {sp = s};
         if (j < pos) {pos = j};
-        if ((j % 10) < (pos % 10)) {pos = pos - (pos % 10 - j % 10)};
+        if ((j % stulp) < (pos % stulp)) {pos = pos - (pos % stulp - j % stulp)};
     }
-
-
-    console.log("sena pozicija" + pos);
-
     for (var i = 0; i < judaX.length; i++) {
         judaX[i] -= pos;
     }
-
-    let a = stulp - 1 - pos;  /*atstumas nuo sieneles*/
+    let a = stulp - 1 - (pos % stulp);  /*atstumas nuo sieneles*/
     let b = Math.floor(judaX[3] / stulp) - Math.floor(judaX[0] / stulp); /*kalades aukstis*/
-
     if (a < b) {
         pos = pos - (b - a);
     }
-
-    console.log("nauja pozicija" + pos);
-
     for (let i = 0; i < kalades.length; i++) {
         if (kalades[i][0] == judaX[0] && kalades[i][1] == judaX[1] && kalades[i][2] == judaX[2] && kalades[i][3] == judaX[3]) {
             kaladesNr = i;
-            console.log("radom");
         }
     }
-
     if (kaladesNr % 4 == 3) {
         kaladesNr -= kaladesNr % 4;
     } else {
         kaladesNr++;
     }
-
-    console.log("nauja kalade: " + kaladesNr);
-
     for (let i = 0; i < 4; i++) {
         judaY[i] = kalades[kaladesNr][i] + pos;
     }
-    
     padetiKalade(judaY, sp);
+}
+
+function leistis() {
+        prieDugno = false;
+        let sp;
+        let z = 4;  /*po paspaudimo leisimes 4 eilutes zemyn*/
+
+    do {
+        let judaX = [];
+        let judaY = [];
+        prieDugno = false;
+
+        for (i = 0; i < 4; i++) {
+            let j = $(".juda").attr("id");
+            $("#"+j).removeClass("juda");
+            let s = $("#"+j).attr("class");
+            $("#"+j).removeClass(s);
+            j = parseInt(j);
+            judaX.push(j);
+            if (i == 0) {sp = s};
+        }
+        judaY = judaX;
+
+        for (let i = 0; i < 4; i++) {
+            if (Math.ceil(judaY[i]) / stulp >= eil - 1) {prieDugno = true;}
+            let nid = judaY[i] + stulp;
+            prieDugno = prieDugno || $("#"+nid).hasClass("guli");
+        }
+
+        if (!prieDugno) {
+            for (let i = 0; i < judaY.length; i++) {
+                judaY[i] = judaY[i]+stulp;
+            };
+            padetiKalade(judaY, sp);
+        } else {
+                paguldyti(judaY, sp);
+            };
+        z--;
+    }
+    while (!prieDugno && z > 0);
+}
+
+function paguldyti (kalade, spalva) {
+    console.log("guldom");
+    console.log(kalade);
+    for (i = 0; i < 4; i++) {
+        $("#"+kalade[i]).removeClass();
+        $("#"+kalade[i]).addClass("guli");
+        $("#"+kalade[i]).addClass(spalva);
+        let gulintiEilute = Math.floor(kalade[i] / stulp);
+        let eilutePilna = true;
+        console.log("tikrinam eilute: " + gulintiEilute);
+        for (let k = 0; k < stulp; k++) {
+            let elem = gulintiEilute * stulp + k;
+            eilutePilna = eilutePilna && $("#"+elem).hasClass("guli");
+        };
+        if (eilutePilna) {
+            console.log("uzskaitom");
+            for (let k = gulintiEilute * stulp; k < gulintiEilute * (stulp + 1) ; k++) {
+                $('#'+k).removeClass();
+            }
+            let l = 0;
+            let m = gulintiEilute * stulp - 1;
+            let o = [];
+            while (l <= stulp) {
+                if ($("#"+m).hasClass("guli")) {
+                    $("#"+m).removeClass("guli");
+                    let s = $("#"+m).attr("class");
+                    $("#"+m).removeClass(s);
+                    let p = {id:m, sp:s};
+                    o.push(p);
+                    l = 0;
+                } else {
+                    l++
+                };
+                m--;
+            }
+            console.log(o);
+            for (let i = 0, j = o.length; i < j; i++) {
+                let naujasid = o[i].id + stulp;
+                let s = o[i].sp;
+                $("#"+naujasid).addClass("guli");
+                $("#"+naujasid).addClass(s);
+            }
+        };
+    };
+    gautiKalade();
 }
 
 
